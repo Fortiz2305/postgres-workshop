3b57377e05f3aa2e89f774e23d6b83c0e9d7ac94
jorge jardines <jorge@themotion.com>
2017-07-06 19:35:12 +0200

    batch_insert first draft implementation

---
+++ bulkload/batch_insert.py
@@ -0,0 +1,37 @@
+import csv
+import logging
+from db import PostgresDb
+from measure import measure
+
+# http://initd.org/psycopg/docs/extras.html#fast-exec
+
+@measure
+def insert_line_by_line_prepared_statements(logger):
+    with open('../resources/2015-Q2-Trips-History-Data.csv') as trips_csv:
+        reader = csv.reader(trips_csv)
+        # ignore header
+        next(reader)
+        try:
+            conn = PostgresDb('workshop', 'postgres','db', 'postgres')
+            # prepared_statement = "PREPARE bulkplan as INSERT INTO trips VALUES ($1, $2, $3, $4, $5, $6, $7)"
+            # conn.execute(prepared_statement, disconnect=False)
+            arg_list = []
+            for idx, row in  enumerate(reader):
+                arg_list.append((row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+                if idx % 1000 == 0:
+                    # conn.execute_values("execute bulkplan %s", arg_list, disconnect=False)
+                    conn.execute_values("INSERT INTO trips VALUES %s", arg_list, page_size=1000, disconnect=False)
+                    logger.info("1000 registros insertados")
+                    arg_list = []
+            else:
+                if arg_list:
+                    conn.execute_values("execute bulkplan %s", arg_list,
+                    page_size=10, disconnect=False)
+        finally:
+            logger.info('closing connection ...')
+            conn.disconnect()
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s.%(msecs)03d:%(levelname)s - %(message)s', datefmt='%Y-%m-%d,%H:%M:%S', level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    insert_line_by_line_prepared_statements(logger)
